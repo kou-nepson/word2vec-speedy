46ef57ceee019b564a87a78b94050392e2d2d6db
杉山航 <cjgjc803@gmail.com>
2018-11-21 11:12:38 +0900

    インポートエラーを解消

--- layers.py
+++ layers.py
@@ -1,7 +1,7 @@
 # coding: utf-8
-from common.np import *  # import numpy as np
-from common.config import GPU
-from common.functions import softmax, cross_entropy_error
+import numpy as np#from np import *  # import numpy as np
+#from config import GPU
+from functions import softmax, cross_entropy_error
 
 
 class MatMul:
